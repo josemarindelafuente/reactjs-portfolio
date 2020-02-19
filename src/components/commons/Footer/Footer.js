3c66dc3875afeeda636ae9689cb470d2706f9fce
jose Marin <jose.marindelafuente@gmai.com>
2020-02-19 09:33:24 -0300

    modificaciones en el footer

--- src/components/commons/Footer/Footer.js
+++ src/components/commons/Footer/Footer.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Footer.scss';
+import LogoFooter from '../../../assets/logo-footer.png';
 
 const Footer = () => {
     return ( 
         <div className='contenedor_footer'>
             
             <div className="container">
-                Footer
+                <div className="row">
+
+                    <div className="col-12 col-md-6 footer_col_iz">
+                        <img src={LogoFooter} width="250" height="44" alt="Marin de la fuente"/>
+                    </div>
+
+                    <div className="col-12 col-md-6 footer_col_de">
+                    <p>Visita mi sitio Web <a href="https://www.marindelafuente.com.ar" target="_blank" >www.marindelafuente.comar</a>
+                    </p>
+                    </div>
+
+                    
+                </div>               
             </div>
 
         </div>
